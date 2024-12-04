0e61cf4ccc8e76dbe1843e620a777c33dce28638
Patsagorn <patsagorn_y@outlook.com>
2024-12-04 21:43:07 +0700

    day 4 part 1 with debug logging

---
+++ 2024/04/day04.ts
@@ -0,0 +1,63 @@
+export function parse(input: string) {
+  return input.split('\n').map(line => line.split(""))
+}
+
+const isValidCoord = (x: number, y: number, m: number, n: number) => x >= 0 && x < m && y >= 0 && y < n
+const directions = [
+  [-1, -1], [0, -1], [1, -1],
+  [-1, 0], [1, 0],
+  [-1, 1], [0, 1], [1, 1],
+]
+
+const arrowsDirections = [
+  "↖", "↑", "↗",
+  "←", "→",
+  "↙", "↓", "↘",
+]
+
+const dirToArrow = (dir: number[]) => {
+  const index = directions.findIndex(d => d[0] === dir[0] && d[1] === dir[1])
+  return arrowsDirections[index] + ` (${dir[0]},${dir[1]})`
+}
+
+export function partOne(input: ReturnType<typeof parse>) {
+  let count = 0;
+  const rowCount = input.length;
+  const colCount = input[0].length;
+  const word = "XMAS"
+  for (let y = 0; y < rowCount; y++) {
+    for (let x = 0; x < colCount; x++) {
+      for (const dir of directions) {
+        if (input[y][x] !== word[0]) continue
+        let nx = x + dir[0]
+        let ny = y + dir[1]
+        let wordIndex = 1;
+        console.log(`\n[new] start X at (${x},${y}) [${dirToArrow(dir)}]`)
+        while (nx >= 0 && nx < colCount && ny >= 0 && ny < rowCount) {
+          if (input[ny][nx] !== word[wordIndex]) {
+            console.log(`  heh, (${nx},${ny}) not match got ${input[ny][nx]} but should be ${word[wordIndex]}`)
+            break
+          }
+          console.log(`  found ${input[ny][nx]}! at (${nx},${ny}) [${dirToArrow(dir)}]`)
+          nx += dir[0]
+          ny += dir[1]
+          wordIndex += 1
+        }
+        if (wordIndex === word.length) {
+          count++
+          console.table({
+            start: [x, y],
+            end: [nx, ny],
+            dir: dirToArrow(dir)
+          })
+        }
+      }
+      console.log("=====================================")
+    }
+  }
+  return count
+}
+
+export function partTwo(input: ReturnType<typeof parse>) {
+
+}
